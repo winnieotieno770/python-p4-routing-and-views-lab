23968a3e5b5a41157c2f232919f67b2eb670e315
winnieotieno770 <winnieotieno770@gmail.com>
2026-01-28 13:34:26 +0300

    Instantiate flask app and create routes

--- server/app.py
+++ server/app.py
@@ -4,5 +4,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route("/print/<string:user>")
+def print_string(user):
+    print(user)
+    return f"{user}"
+
+@app.route("/count/<int:num>")
+def count(num):
+     result = ""
+     for i in range(num):
+        result += f"{i}\n"
+     return result
+@app.route('/math/<int:num>/<string:opt>/<int:num2>')
+def math(num,opt,num2):
+     if opt == '+':
+        result = num + num2
+     elif opt == '-':
+         result = num - num2
+     elif opt == '*':
+         result = num * num2
+     elif opt == '%':
+         result = num % num2
+     elif opt == 'div': 
+         result = num / num2
+     else:
+         return 0
+     return str(result)
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
